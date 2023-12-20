91a17d59321b3b22bed016451f5ed7f378b7ce4c
vinodmadarapu <126435578+vinodmadarapu@users.noreply.github.com>
2023-12-20 16:42:05 -0600

    Update pipeline.sh

--- cicd/pipeline.sh
+++ cicd/pipeline.sh
@@ -3,13 +3,14 @@ echo "start process pipeline"
 cp ./cicd/settings.xml settings.xml
 
 JFROG_URL=https://svk2015.jfrog.io/artifactory
-JFROG_COMMAND=${GITHuB_WORKSPACE}/jfrog
+JFROG_COMMAND=${GITHUB_WORKSPACE}/jfrog
 JFROG_DEFAULT_REPO=dev-releases
 
 
 #insall jfrog
 curl -sS -fL https://getcli.jfrog.io | bash -s v2 "2.46.2"
-$JFROG_COMMAND --version 
-$JFROG_COMMAND config add --artifactory-url=${JFROG_URL} --access-token=cmVmdGtuOjAxOjE3MzQ2NDQzMTU6YzJybVRCUFFTeXZFZDBub0lBSHEzWXBaWWVP
-$JFROG_COMMAND config show
+jfrog --version
+# $JFROG_COMMAND --version 
+# $JFROG_COMMAND config add --artifactory-url=${JFROG_URL} --access-token=cmVmdGtuOjAxOjE3MzQ2NDQzMTU6YzJybVRCUFFTeXZFZDBub0lBSHEzWXBaWWVP
+# $JFROG_COMMAND config show
 
