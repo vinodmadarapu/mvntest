e8117e271af451862247056200cfbe439fdfd825
vinodmadarapu <126435578+vinodmadarapu@users.noreply.github.com>
2023-12-25 06:11:47 -0600

    Update pipeline.sh

--- cicd/pipeline.sh
+++ cicd/pipeline.sh
@@ -5,7 +5,7 @@ cp ./cicd/settings.xml settings.xml
 
 JFROG_URL=https://svk2015.jfrog.io/artifactory
 JFROG_COMMAND=${GITHUB_WORKSPACE}/jfrog
-JFROG_DEFAULT_REPO=my-mvn-virtual-releases
+JFROG_DEFAULT_REPO=my-mvn-virtual-snapshots
 
 POM_VERSION=$(mvn --batch-mode -s settings.xml  help:evaluate -Dexpression=project.version -q -DforceStdout)
 POM_ARTIFACT_ID="jb-hello-world-maven"
