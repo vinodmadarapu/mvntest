67d477631295177224dc9d0d2f3516e9d84f9805
vinodmadarapu <126435578+vinodmadarapu@users.noreply.github.com>
2023-12-26 14:40:34 -0600

    Update deploy.sh

--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -4,8 +4,8 @@
 TENENTID="64ceb8ab-f04f-477d-8057-7dfdacb83dcb"
 RG=RG1
 SU="Azure subscription 1"
-USERNAME=satya.madarapu23@gmail.com
-PASSWORD=Sansat@2k
+USERNAME=fa1085d1-a654-4325-8a45-6d5b9fe0534d
+PASSWORD=cND8Q~oYKod4L0RPwxrNEKjCSbRiqLsqt8ZItdB7
 
 az login --service-principal --username=${USERNAME} --password=${PASSWORD} --tenant=${TENENTID}
 az account set --subscription ${SU}
