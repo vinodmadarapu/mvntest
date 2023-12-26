585df5435bccd4c59b7a34381c1217ca6e440f2a
vinodmadarapu <126435578+vinodmadarapu@users.noreply.github.com>
2023-12-26 12:46:35 -0600

    Update deploy.sh

--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -1,9 +1,15 @@
 #!/bin/bash -e
 
 #azure login
-az login --service-principal --username="vinod" --password="password" --tenant=tenantID"
-az account set --subscription "subscription"
+TENENTID="64ceb8ab-f04f-477d-8057-7dfdacb83dcb"
+RG=RG1
+SU="Azure subscription 1"
+USERNAME=satya.madarapu23@gmail.com
+PASSWORD=Sansat@2k
 
-az webapp deploy --resource-group= --name= --src-path=init-sh --type="static"  --target-path="init.sh"
-az webapp deploy source config-zip --resource-group= --name=  --src=  --debug
-az webapp restart --resource-group= --name=  --debug
+az login --service-principal --username=${USERNAME} --password=${PASSWORD} --tenant=${TENENTID}
+az account set --subscription ${SU}
+
+# az webapp deploy --resource-group= --name= --src-path=init-sh --type="static"  --target-path="init.sh"
+# az webapp deploy source config-zip --resource-group= --name=  --src=  --debug
+# az webapp restart --resource-group= --name=  --debug
