b5e093da76da02c6dae010b1560d2b61fe12c077
vinodmadarapu <126435578+vinodmadarapu@users.noreply.github.com>
2023-12-27 11:16:48 -0600

    Update deploy.sh

--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -38,6 +38,8 @@ artifact_dir="artifact"
 mkdir $artifact_dir
 $JFROG_COMMAND rt dl "*${JFROG_PROD_REPO}/${GROUP_ID_Replaced}/${POM_ARTIFACT_ID}*.jar" $artifact_dir/ --sort-by created --sort-order=desc --limit=1
 # echo prodjar/${GROUP_ID_Replaced}/${POM_ARTIFACT_ID}/*/*.jar > ./cicd/buildjarNameFile.txt
+deploy_zip_name="azure_artifacts.zip"
+zip -j ${deploy_zip_name} $artifact_dir/${GROUP_ID_Replaced}/${POM_ARTIFACT_ID}*.jar
 cd $artifact_dir
 pwd
 ls -l
